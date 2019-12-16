d47545968a956683edbd1b4fea15bc97ee5f9d1a
Tianyi GAO <scytg1@exmail.nottingham.ac.uk>
2019-12-16 01:02:24 +0000

    style: tab to 2 spaces

--- src/main/java/frogger/Main.java
+++ src/main/java/frogger/Main.java
@@ -5,44 +5,46 @@ import frogger.util.score.ScoreManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-/** {@link Main} class for frogger game application. */
+/**
+ * {@link Main} class for frogger game application.
+ */
 public class Main extends Application {
 
-	/** Primary stage for this application. **/
-	private static Stage primaryStage;
+  /**
+   * Primary stage for this application.
+   **/
+  private static Stage primaryStage;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-	/**
-	 * Getter for {@link #primaryStage}
-	 *
-	 * @return primary stage of this application
-	 */
-	public static Stage getPrimaryStage() {
-		return primaryStage;
-	}
+  /**
+   * Getter for {@link #primaryStage}
+   *
+   * @return primary stage of this application
+   */
+  public static Stage getPrimaryStage() {
+    return primaryStage;
+  }
 
-	/**
-	 * Start the game application
-	 *
-	 * <p>Assign the primary stage and set stage not sizable and its title.</p>
-	 * <p>Initialize {@link ScoreManager} and switch scene to home by calling {@link SceneSwitch#switchToHome()}</p>
-	 *
-	 * @param primaryStage primary stage of this application
-	 */
-	@Override
-	public void start(Stage primaryStage) {
-		Main.primaryStage = primaryStage;
+  /**
+   * Start the game application
+   *
+   * <p>Assign the primary stage and set stage not sizable and its title.</p>
+   * <p>Initialize {@link ScoreManager} and switch scene to home by calling {@link SceneSwitch#switchToHome()}</p>
+   *
+   * @param primaryStage primary stage of this application
+   */
+  @Override
+  public void start(Stage primaryStage) {
+    Main.primaryStage = primaryStage;
 
-		primaryStage.setResizable(false);
-		primaryStage.setTitle("frogger");
-
-
-		ScoreManager.INSTANCE.init();
-		SceneSwitch.INSTANCE.switchToHome();
-	}
+    primaryStage.setResizable(false);
+    primaryStage.setTitle("frogger");
 
 
+    ScoreManager.INSTANCE.init();
+    SceneSwitch.INSTANCE.switchToHome();
+  }
 }
